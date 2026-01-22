a886b90c73d9e2d4c203164448dc3330cc45eb96
Parthasarathy-G <parthasarathy0580@gmail.com>
2026-01-22 21:48:40 +0530

    Add loop.py with examples of loops and commented-out code for pin validation

---
+++ Week_1/Day_2/loop.py
@@ -0,0 +1,16 @@
+# names = ["Partha", "Gowtham", "Kumar"]
+# for test in names:
+#     print(test.upper())
+
+# correct_pin = '1234'
+# entered_pin = ''
+# while entered_pin != correct_pin:
+#     entered_pin = input("Enter your correct pin: ")
+#     if (entered_pin != correct_pin):
+#         print("Wrong pin")
+# print("Access granted")
+
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
