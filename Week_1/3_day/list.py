698a1c7b668107f657a9ce72fa94fe18c6646ab0
Parthasarathy-G <parthasarathy0580@gmail.com>
2026-01-23 15:29:39 +0530

    Add multiple new files for Week 1 exercises including billing, command, hotel, main, operations, typescript, condition statements, input handling, loops, string manipulation, and Day 3 functions, args, cart, countdown, kargs, list, and test scripts.

---
+++ Week_1/3_day/list.py
@@ -0,0 +1,45 @@
+food = ["Briyani", "Parotta", "Fried rice"]
+pc = ["Speaker", "Mouse", "Monitor"]
+location = ["Coimbatore", "Chennai", "Tirunellveli"]
+
+# print("Food list available: ", food)
+# print("Pc components list available: ", pc)
+# print("Location list: ", location)
+
+#list methods
+
+# food.append("Grilled chicken")
+# print("After append: ", food)
+
+# food.insert(1,"Grilled chicken")
+# print("After insert: ", food)
+
+
+# food.remove("Grilled chicken")
+# print("After removed: ", food)
+
+# food.pop()
+# print("After pop: ", food)
+
+# food.reverse()
+# print("After reverse: ", food)
+
+# print("Count", food.count("Parotta"))
+
+#list slicing
+
+# print("Top location", location[0:1])
+
+# List iteration
+
+# for i  in food:
+#     print("All food",i)
+
+# if "Parotta" in food:
+#     print("yes")
+
+# food[1] = "Shawarma"
+# print(food)
+
+mixed = ["Parthasarathy", 23, 1.78]
+print(mixed)
