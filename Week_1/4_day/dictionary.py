3527c4e297bdb01fe25e84ccfed9bf81f966a8fb
Parthasarathy-G <parthasarathy0580@gmail.com>
2026-01-24 16:26:46 +0530

    Add initial implementations for dictionary and set examples, and create a temporary runner file

---
+++ Week_1/4_day/dictionary.py
@@ -0,0 +1,20 @@
+trip = {
+    "Trip_id" : "PS282002",
+    "Pickup": "Coimbatore Railway station",
+    "Drop" : "Peiyanaickepalayam",
+    "Fare" : 450.75,
+    "Status" : "Completed"
+}
+
+# print(trip.get("Pickup"))
+# print(trip.keys())
+# print(trip.values())
+
+# for key, values in trip.items():
+#     print(key,": ",values)
+
+# trip.update({"Car_model" : "Swift dzire"})
+# print(trip)
+
+# trip.pop("status")
+
---
+++ Week_1/4_day/set.py
@@ -0,0 +1,24 @@
+# a = ["Parthsarathy","Kumar","Hari","Kumar"]
+
+# unique_name = set(a)
+# print(unique_name)
+
+# red_taxi1 = {"Coimbatore", "Madurai"}
+# red_taxi2 = {"Bangalore", "Chennai","Madurai"}
+
+# print(red_taxi1.union(red_taxi2))
+# print(red_taxi1.intersection(red_taxi2))
+
+# red_taxi2.add("Salem")
+# print(red_taxi2)
+
+# red_taxi2.remove("Madurai")
+# print(red_taxi2)
+
+# print(red_taxi2[1])
+
+my_set = {1,2,3}
+print(my_set)
+my_set.discard(2)
+my_set.add(99)
+print(my_set)
