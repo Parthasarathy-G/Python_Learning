ae231381c67a575d5c7bcfe87ddb210d0a525583
Parthasarathy-G <parthasarathy0580@gmail.com>
2026-01-25 20:30:22 +0530

    Multiple and multi level inheriance file created and worked on with few examples

--- Week_1/5_day/oops/inheritance.py
+++ Week_1/5_day/oops/inheritance.py
@@ -1,7 +1,50 @@
-class dad: # parent
-    def house(self):
-        print ("red")
+# class grandfather(self):
+
+#     def car(self):
+#         print("red car")
+
+# Multiple inheritance
+# class dad(): # parent
+#     def house(self):
+#         print ("house white")
+
+# class son1 (dad):
+
+#     def factory(self):
+#         print("Factory")
+# class son2 (dad):
+
+#     def market(self):
+#         print("market")
     
+# g = son1()
+# g.house()
+# g.factory()
+
+# v = son2()
+# v.house()
+# v.market()
+
+#Multiple level inheritance
+class dad(): # parent
+    def house(self):
+        print ("house white")
+class mom(): # parent
+    def car(self):
+        print ("red car")
+
+class son1 (dad, mom): # can give two parent class  
+
+    def factory(self):
+        print("Factory")
+    
+g = son1()
+g.house()
+g.factory()
+g.car()
+
+
+
 
 # class dad: # parent
 #     def house(self):
---
+++ Week_1/5_day/oops/tempCodeRunnerFile.py
@@ -0,0 +1,20 @@
+# class dad(): # parent
+#     def house(self):
+#         print ("house white")
+
+# class son1 (dad):
+
+#     def factory(self):
+#         print("Factory")
+# class son2 (dad):
+
+#     def market(self):
+#         print("market")
+    
+# g = son1()
+# g.house()
+# g.factory()
+
+# v = son2()
+# v.house()
+# v.market()
