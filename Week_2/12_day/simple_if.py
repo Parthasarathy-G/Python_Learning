a6cbb20a8c260c262da4b25a884ca0214480d160
Parthasarathy-G <parthasarathy0580@gmail.com>
2026-02-25 18:51:18 +0530

    Add assignments for Module 2: Control Flow, including simple if and if-else statements

---
+++ Week_2/12_day/simple_if.py
@@ -0,0 +1,7 @@
+# Assignment 1: Simple if Statement
+
+# todo - Write a program that asks the user to input a number and prints whether the number is positive.
+
+user = input()
+if(user>0):
+    print("Positive")
