d7584b5b472c3c00cdf0ab7c3b9f5fa8596f0553
Parthasarathy-G <parthasarathy0580@gmail.com>
2026-03-13 09:07:04 +0530

    feat(week4): add Flask form template and update app settings/routes

--- Week_4/25/Flask/main.py
+++ Week_4/25/Flask/main.py
@@ -1,9 +1,5 @@
 from flask import Flask,render_template,request
-'''
- It creates an instance of the Flask class, 
- which will be your WSGI (Web Server Gateway Interface) application.
-'''
-###WSGI Application
+
 app=Flask(__name__)
 
 @app.route("/")
@@ -17,10 +13,13 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 @app.route('/form',methods=['GET','POST'])
 def form():
-        if request.method == 'POST':
-            pass
-        return render_template('form.html')
+    if request.method=='POST':
+        name=request.form['name']
+        return f'Hello {name}!'
+    return render_template('form.html')
+
 if __name__=="__main__":
     app.run(debug=True)
